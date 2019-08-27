481977cb978acf94cb2d55743573e4ff4c84e66a
fcc117 <fieldgar29@gmail.com>
2019-08-27 16:01:43 -0500

    se agrega linea 2

--- DemoSolution/DemoSolution/Program.cs
+++ DemoSolution/DemoSolution/Program.cs
@@ -11,6 +11,7 @@ namespace DemoSolution
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo Git");
+            Console.WriteLine("Hola mundo Git");
         }
     }
 }
